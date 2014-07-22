e0f88b9ed477aff3f49f2acbe4b8eafceeaf81b0
Jay Shah <hey@jay.sh>
2014-07-22 21:14:53 +0100

    Update README and wrap code logic in try/catch to stop it falling over if WhatsApp updates itself.

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
 xposed-whatsapp
 ===========
 
-Utilising the [Xposed framework](http://repo.xposed.info/module/de.robv.android.xposed.installer), this module adds tweaks to make WhatsAp more useful to me. Currently, it allows you to hide the camera and/or voice message shortcuts (independently), but that could change as I discover more changes I'd like to see in WhatsApp that they are unlikely to implement.
+Utilising the [Xposed framework](http://repo.xposed.info/module/de.robv.android.xposed.installer), this module adds tweaks to make WhatsApp more useful to me. Currently, it allows you to hide the camera and/or voice message shortcuts (independently), but that could change as I discover more changes I'd like to see in WhatsApp that they are unlikely to implement.
 
 Removal of the camera and/or voice buttons
 ---------------------------------------
 
-Some time ago, WhatsApp added the "audio voice" feature. I found it hindered significantly more than it helped: if you tapped it accidentally, it would vibrate and emit a notification-like sound. And, I would do this repeatedly.
+Some time ago, WhatsApp added the "audio voice" feature. I found it hindered significantly more than it helped: if you tapped it accidentally, it would vibrate and emit a notification-like sound. And, I would do this repeatedly and so never found this useful (I don't use voice messaging).
 
 More time passes, and they add a camera icon. This is more useful, and personally I'd prefer if that was the only one. So - I wrote this.
 
-#### Before (with both camera and voice visisble):
+#### Before (with both camera and voice visible):
 ![Before](https://raw.githubusercontent.com/jaysh/xposed-whatsapp/master/documentation/images/before.png)
 
 #### After (no voice, no camera):
@@ -22,10 +22,10 @@ More time passes, and they add a camera icon. This is more useful, and personall
 #### After (no camera):
 ![After](https://raw.githubusercontent.com/jaysh/xposed-whatsapp/master/documentation/images/after-no-camera.png)
 
-(*Note:* in all cases, the "send message" button will appear after you start typing, as it always would.)
+(*Note:* in all cases, the "send message" button will appear after you start typing, as it always would. Additionally, disabling sending still allows you to receive them.)
 
 I have tested it on my HTC One M7, using Android Revolution HD, but I have no reason to suspect it shouldn't work on other devices.
 
 ### Troubleshooting
 
-If it feels like the module is doing nothing for you, please try enabling debugging mode from the settings, and send me the logs.
+If it feels like the module is doing nothing for you, please try enabling debugging mode from the settings, and send me the logs via the support thread: http://forum.xda-developers.com/xposed/modules/mod-whatsapp-mods-remove-action-t2824732
--- src/sh/jay/xposed/whatsapp/RemoveActionShortcuts.java
+++ src/sh/jay/xposed/whatsapp/RemoveActionShortcuts.java
@@ -2,6 +2,7 @@ package sh.jay.xposed.whatsapp;
 
 import java.util.Calendar;
 
+import android.util.Log;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
@@ -27,6 +28,7 @@ public class RemoveActionShortcuts implements IXposedHookInitPackageResources {
         XSharedPreferences prefApps = new XSharedPreferences(PACKAGE_NAME);
         prefApps.makeWorldReadable();
 
+        // TODO: Not DRY. Get XSharedPreferences to read the defaults from XML.
         this.debugMode = prefApps.getBoolean("whatsapp_remove_action_shortcuts_debug", false);
         this.removeCameraPreference = prefApps.getBoolean("whatsapp_remove_action_shortcuts_camera", true);
         this.removeVoicePreference = prefApps.getBoolean("whatsapp_remove_action_shortcuts_voice", true);
@@ -52,29 +54,37 @@ public class RemoveActionShortcuts implements IXposedHookInitPackageResources {
                 debug("=================== Start of Conversation layout inflated callback ===================");
                 
                 if (removeVoicePreference) {
-                	ImageButton voiceButton = (ImageButton) liparam.view.findViewById(
-	                        liparam.res.getIdentifier("voice_note_btn", "id", WHATSAPP_PACKAGE_NAME)
-	                );
-	                if (null == voiceButton) {
-	                    debug("Could not locate the voice button, skipping removal");
-	                } else {
-	                    FrameLayout.LayoutParams frameLayoutParams = new FrameLayout.LayoutParams(0, 0);
-	                    voiceButton.setLayoutParams(frameLayoutParams);
-	                }
+                	try {
+	                	ImageButton voiceButton = (ImageButton) liparam.view.findViewById(
+		                        liparam.res.getIdentifier("voice_note_btn", "id", WHATSAPP_PACKAGE_NAME)
+		                );
+		                if (null == voiceButton) {
+		                    debug("Could not locate the voice button, skipping removal");
+		                } else {
+		                    FrameLayout.LayoutParams frameLayoutParams = new FrameLayout.LayoutParams(0, 0);
+		                    voiceButton.setLayoutParams(frameLayoutParams);
+		                }
+                	} catch (Exception e) {
+                		debug("Failed to hook voice button: " + Log.getStackTraceString(e));
+                	}
                 } else {
                 	debug("Not removing voice button because that's what the user preference indicated");
                 }
 
                 if (removeCameraPreference) {
-	                ImageButton cameraButton = (ImageButton) liparam.view.findViewById(
-	                        liparam.res.getIdentifier("camera_btn", "id", WHATSAPP_PACKAGE_NAME)
-	                );
-	                if (null == cameraButton) {
-	                    debug("Could not locate the camera button, skipping removal");
-	                } else {
-	                    LinearLayout.LayoutParams linearLayoutParams = new LinearLayout.LayoutParams(0, 0);
-	                    cameraButton.setLayoutParams(linearLayoutParams);                    
-	                }
+                	try {
+		                ImageButton cameraButton = (ImageButton) liparam.view.findViewById(
+		                        liparam.res.getIdentifier("camera_btn", "id", WHATSAPP_PACKAGE_NAME)
+		                );
+		                if (null == cameraButton) {
+		                    debug("Could not locate the camera button, skipping removal");
+		                } else {
+		                    LinearLayout.LayoutParams linearLayoutParams = new LinearLayout.LayoutParams(0, 0);
+		                    cameraButton.setLayoutParams(linearLayoutParams);                    
+		                }
+                	} catch (Exception e) {
+                		debug("Failed to hook camera button: " + Log.getStackTraceString(e));
+                	}
                 } else {
                 	debug("Not removing camera button because that's what the user preference indicated");
                 }
